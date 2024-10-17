8998b1cb9395d140303c5970156fe89bb22393cc
Md. Alif Babu <muhammad.alifbabu@gmail.com>
2024-10-17 12:24:31 +0600

    initial commit

---
+++ src/main/java/com/example/springdocumentreader/SpringDocumentReaderApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdocumentreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDocumentReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDocumentReaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springdocumentreader/SpringDocumentReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdocumentreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDocumentReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
